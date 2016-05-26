a26574197b95438c34de19d15404bef3f1a8ffec
Nirmal Krishnan <nkrishn9@jhu.edu>
2016-05-26 10:22:47 -0400

    added ID of savings account

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ### API Code to use : 482cef6d93c206eb7e5c9fefc8941ccb
 ### Ayo's ID: 574702718a710f8e12324ee0
+### Ayo's Savings ID: 574704c48a710f8e12324ee1
 
--- datapopulate.py
+++ datapopulate.py
@@ -1,8 +1,27 @@
 import sys
+import requests
+import json
 
 def main():
+	customerId = '574702718a710f8e12324ee0'
+	apiKey = '482cef6d93c206eb7e5c9fefc8941ccb'
 
+	url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId,apiKey)
+	payload = {
+	  "type": "Savings",
+	  "nickname": "Tryna save some dosh",
+	  "rewards": 10000,
+	  "balance": 10000,
+	}
+	# Create a Savings Account
+	response = requests.post(
+		url,
+		data=json.dumps(payload),
+		headers={'content-type':'application/json'},
+		)
 
+	if response.status_code == 201:
+		print('account created')
 
 if __name__ == "__main__":
 	main()
